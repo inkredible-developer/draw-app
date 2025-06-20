387ccb81bca553d08ffbecc8dfe23f2ec335d7e9
Ali An Nuur <alianur043@gmail.com>
2025-06-20 17:52:36 +0700

    MVC

---
+++ wedraw/wedraw/Model/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  wedraw
+//
+//  Created by Ali An Nuur on 20/06/25.
+//
+
+import Foundation
---
+++ wedraw/wedraw/View/View.swift
@@ -0,0 +1,8 @@
+//
+//  View.swift
+//  wedraw
+//
+//  Created by Ali An Nuur on 20/06/25.
+//
+
+import Foundation
